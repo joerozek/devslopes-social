1cb08870d343bf65fdf1de4ffe6739a47c67428a
Joe Rozek <jrozek@gmail.com>
2017-09-11 22:46:03 -0700

    custom UI

---
+++ devslopes-social/FancyButton.swift
@@ -0,0 +1,23 @@
+//
+//  FancyButton.swift
+//  devslopes-social
+//
+//  Created by Joe Rozek on 9/11/17.
+//  Copyright © 2017 Joe Rozek. All rights reserved.
+//
+
+import UIKit
+
+class FancyButton: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_GREY, green: SHADOW_GREY, blue: SHADOW_GREY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 1.0
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        layer.cornerRadius = 2.0
+    }
+
+}
---
+++ devslopes-social/FancyField.swift
@@ -0,0 +1,29 @@
+//
+//  FancyField.swift
+//  devslopes-social
+//
+//  Created by Joe Rozek on 9/11/17.
+//  Copyright © 2017 Joe Rozek. All rights reserved.
+//
+
+import UIKit
+
+class FancyField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.borderColor = UIColor(red: SHADOW_GREY, green: SHADOW_GREY, blue: SHADOW_GREY, alpha: 0.2).cgColor
+        layer.borderWidth = 1.0
+        layer.cornerRadius = 2.0
+        
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10, dy: 5)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10, dy: 5)
+    }
+}
